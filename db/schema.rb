44d6fe58892dab3445da917a5c477356f2d166ea
shoyasono <shoya.ono.125@gmail.com>
2014-10-22 18:01:22 -0700

    アソシエーション構造完成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022210004) do
+ActiveRecord::Schema.define(version: 20141022223028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141022210004) do
     t.date     "posted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "users", force: true do |t|
