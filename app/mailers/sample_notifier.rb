108f069da3835ed8de4200cb276f98e979be7aa7
shoyasono <shoya.ono.125@gmail.com>
2014-10-30 15:45:24 -0700

    SampleNotifierを編集

--- app/mailers/sample_notifier.rb
+++ app/mailers/sample_notifier.rb
@@ -4,9 +4,7 @@ class SampleNotifier < ActionMailer::Base
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  1 <%= Rouge::Themes::Github.render(:scope => '.highlight') %>
-  #   en.sample_notifier.sample_mail.subject
-  #
+
   def sample_mail
     @greeting = "Hi"
 
