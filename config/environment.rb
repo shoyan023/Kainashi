4c50092bdeb79dd4c1d5eb475a9dbe67e106b605
shoyasono <shoya.ono.125@gmail.com>
2014-10-30 14:17:25 -0700

    メルマガ送信の送信部分の実装

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Action Mailer Setting
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["noren.member"],
+  :password => ENV["hotokenonoren3"],
+  :domain => "kainashi.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+ActionMailer::Base.delivery_method = :smtp
+
