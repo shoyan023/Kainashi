a44a026b8ef2aada44b60d18b53e396f9fffd565
shoyasono <shoya.ono.125@gmail.com>
2014-10-31 16:42:52 -0700

    最初にmigrateするmigarationファイルを修正

--- db/migrate/20141026001137_create_stars.rb
+++ db/migrate/20141026001137_create_stars.rb
@@ -3,6 +3,7 @@ class CreateStars < ActiveRecord::Migration
     create_table :stars do |t|
       t.integer :user_id
       t.integer :post_id
+      t.integer :current_user_id
 
       t.timestamps
     end
