0a9aec6c899df79205dbb0140816b89711c9e865
shoyasono <shoya.ono.125@gmail.com>
2014-10-30 18:38:19 -0700

    signupは完成※loginはまだ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,22 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :themes, through: :posts
   has_many :stars, :dependent => :destroy
+  attr_accessor :login
 
   def starable_for?(post)
 post && post.user != self && !stars.exists?(:post_id => post.id)
 end
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   validates :username, presence: true, uniqueness: true
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
