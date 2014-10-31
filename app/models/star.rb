7cfd012b7db36e6d71596fa191bf8d0bb74267e9
shoyasono <shoya.ono.125@gmail.com>
2014-10-31 11:01:31 -0700

    starのバグを解決

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,5 +1,5 @@
 class Star < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  validates_uniqueness_of :post_id || :current_user_id
+  validates_uniqueness_of :post_id, :scope => :current_user_id
 end
