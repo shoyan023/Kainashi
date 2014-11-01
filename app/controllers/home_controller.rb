99a179de9b712a011ce5f831c557f7345cc8ac34
shoyasono <shoya.ono.125@gmail.com>
2014-11-01 13:31:48 -0700

    index#homeのviewが完成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @themes = Theme.all
+    @themes = Theme.last
+    @post = Post.new  #新規ポスト作成用
+    @star = Star.new  #新規ファボ作成
   end
 
   def about
@@ -8,4 +10,35 @@ class HomeController < ApplicationController
 
   def terms
   end
+
+  def new
+    @themes = Theme.find(params[:id])
+    @posts = @themes.posts
+    @star = Star.new  #新規ファボ作成
+    @posts.id = "id"
+  end
+
+  def create
+    @star = Star.new(star_params)
+    if @star.save
+      redirect_to @star, notice: "ユーザ登録しました。"
+    else
+      render 'home#index'
+    end
+  end
+
+  def update
+    @star = Star.new(star_params)
+    if @star.save
+      redirect_to @star, notice: "ユーザ登録しました。"
+    else
+      render 'home#index'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:star).permit(:user_id, :post_id)
+  end
+
 end
