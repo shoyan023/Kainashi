0c6e6e8e89393f0d9e2b99cf6c5221cf24132260
shoyasono <shoya.ono.125@gmail.com>
2014-11-01 20:34:08 -0700

    微修正

---
+++ config/environment.rb
@@ -0,0 +1,18 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+# Action Mailer Setting
+ActionMailer::Base.smtp_settings = {
+  :user_name => "",
+  :password => "",
+  :domain => "kainashi.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+ActionMailer::Base.delivery_method = :smtp
+
