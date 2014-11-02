c754f0e035d726c485cf8259012f5b1ae3570230
shoyasono <shoya.ono.125@gmail.com>
2014-11-01 22:15:14 -0700

    sendgridの環境設定を追加

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ Rails.application.initialize!
 
 # Action Mailer Setting
 ActionMailer::Base.smtp_settings = {
-  :user_name => "",
-  :password => "",
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
   :domain => "kainashi.herokuapp.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
